68fa0821edafff4d359735f27f1c537a7065185f
Motti789 <mchillywilly@gmail.com>
2022-03-09 14:10:54 -0500

    commit tree ec61d246286897954fcd1a4314290114dd0515eb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+  return drivers.filter( possibleMatch => possibleMatch.toLowerCase() === string.toLowerCase()
+  )
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+
+  function matchName(drivers, string)  {
+    return drivers.filter( possibleMatch =>
+        possibleMatch.name === string )
+  }
